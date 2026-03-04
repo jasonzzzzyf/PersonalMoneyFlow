f7001fe6f3d95fa3edff94e95f661648385642a3
JasonFubaoZ <zhangyifei0816@gmail.com>
2026-03-04 11:24:09 -0500

    feat: add Category entity and repository

---
+++ backend/src/main/java/com/jason/personalmoneyflow/model/entity/Category.java
@@ -0,0 +1,41 @@
+package com.jason.personalmoneyflow.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "categories",
+        indexes = {
+                @Index(name = "idx_categories_user_id", columnList = "user_id")
+        }
+)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
+}
+
